4bc990c9b949c124f055b34f9e277d55c46d13c2
pcreis2004 <pcreis2011@gmail.com>
2025-03-03 16:17:49 +0000

    Alteração da função do inicialização da socket de init para ficar mais de acordo com o que demos no lab

--- ndn.c
+++ ndn.c
@@ -70,6 +70,7 @@ int main(int argc, char *argv[]){
 	printf("NDN Node started. Enter commands:\n");
     while (1) {
         printf("> ");
+        //MELHORAR O LOOP
 		counter=select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict exceptfds, struct timeval *restrict timeout);//completar
 		for (i=0; i<counter ; i++) {
 
@@ -144,4 +145,24 @@ int init_socket_listening(int port,char ip[16]) {
     }
     
     return sock;
+}
+
+int init_socket_listeningALT(int port, char ip[16]){
+
+    struct addrinfo hints,*res;
+    int fd,newfd,errcode; ssize_t n,nw;
+    struct sockaddr addr; socklen_t addrlen;
+    char *ptr,buffer[128];
+
+    if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)exit(1);//error
+
+    memset(&hints,0,sizeof hints);
+
+    hints.ai_family=AF_INET;//IPv4
+    hints.ai_socktype=SOCK_STREAM;//TCP socket
+    hints.ai_flags=AI_PASSIVE;
+    
+    
+    if((errcode=getaddrinfo(ip,port,&hints,&res))!=0)/*error*/exit(1);
+
 }
